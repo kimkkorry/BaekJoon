a601ac2309a0b0cb35219a9aa9b0d8c06cc17ee6
Kim TaeIl <151612819+kimkkorry@users.noreply.github.com>
2024-01-10 13:02:04 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        if (num == 1)
+            answer = 0;
+        else{
+            while (num != 1){
+                if (num%2==0){
+                    num = num/2;
+                    answer +=1;
+                }
+                else{
+                    num = num*3 + 1;
+                    answer +=1;
+                    }
+                if (answer >500){
+                    answer = -1;
+                    break;
+                }
+            }
+            
+        }
+        return answer;
+    }
+}
