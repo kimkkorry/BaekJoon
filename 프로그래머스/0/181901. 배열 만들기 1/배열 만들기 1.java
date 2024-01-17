b12e1f4ecc855caa6917d3ee12f0644dd3f7c512
Kim TaeIl <151612819+kimkkorry@users.noreply.github.com>
2024-01-18 01:48:26 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int n, int k) {
+        int[] answer = new int[n/k];
+        for (int i=k; i<=n; i+=k)
+            answer[i/k-1] = i;
+        return answer;
+    }
+}
