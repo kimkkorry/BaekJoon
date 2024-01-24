886fcde2fdaff4a83782a2380363c8839265cfbf
Kim TaeIl <151612819+kimkkorry@users.noreply.github.com>
2024-01-24 14:13:05 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] numbers, String direction) {
+        int[] answer = new int [numbers.length];
+        if (direction.equals("left")){
+            for (int i=1; i<numbers.length;i++)
+                answer[i-1] = numbers[i];
+            answer[answer.length-1] = numbers[0];
+        }
+        else{
+            answer[0] = numbers[numbers.length-1];
+            for (int i=1; i<numbers.length;i++)
+                answer[i] = numbers[i-1];
+        }
+        return answer;
+    }
+}
