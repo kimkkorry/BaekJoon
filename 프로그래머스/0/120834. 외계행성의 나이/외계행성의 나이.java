7fbc25341b51319e6aa80366b07b46f929484135
Kim TaeIl <151612819+kimkkorry@users.noreply.github.com>
2024-01-24 14:16:30 +0900

    [level 0] Title: 외계행성의 나이, Time: 8.22 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        String a[] = (age+"").split("");
+        for (String aa : a){
+            switch(aa){
+                case "0" :
+                    answer += "a";
+                    break;
+                    case "1" :
+                    answer += "b";
+                    break;
+                    case "2" :
+                    answer += "c";
+                    break;
+                    case "3" :
+                    answer += "d";
+                    break;
+                    case "4" :
+                    answer += "e";
+                    break;
+                    case "5" :
+                    answer += "f";
+                    break;
+                    case "6" :
+                    answer += "g";
+                    break;
+                    case "7" :
+                    answer += "h";
+                    break;
+                    case "8" :
+                    answer += "i";
+                    break;
+                    case "9" :
+                    answer += "j";
+                    break;
+            }
+        }
+        return answer;
+    }
+}
