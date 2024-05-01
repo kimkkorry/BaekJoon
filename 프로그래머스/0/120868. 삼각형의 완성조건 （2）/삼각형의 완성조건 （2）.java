c7e01a5464512866d6111f1e6a7d2c7f8cb1178f
Kim TaeIl <151612819+kimkkorry@users.noreply.github.com>
2024-05-01 14:30:47 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        int min = 0;
+        int max = 0;
+        if (sides[0]>sides[1]){
+            max = sides[0];
+            min = sides[1];
+        }
+        else {
+            max = sides[1];
+            min = sides[0];
+        }
+        for (int i = max-min+1; i<max+min; i++)
+            answer +=1;
+        return answer;
+    }
+}
